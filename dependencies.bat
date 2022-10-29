5837e9793d5c079127edd44c41b830dffd9da0b9
Goofgoof43 <91701399+Goofgoof43@users.noreply.github.com>
2022-10-29 17:16:29 -0400

    Update dependencies.bat

--- dependencies.bat
+++ dependencies.bat
@@ -8,3 +8,4 @@ haxelib git polymod https://github.com/larsiusprime/polymod.git
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
 haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
+haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
